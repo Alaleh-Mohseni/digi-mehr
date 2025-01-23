3ce95ff73da2b069503f076f116ef787525d32ba
mahdieh <madidgn82@gmail.com>
2025-01-23 21:33:04 +0330

    Edit style page login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,17 +2,17 @@ import Image from 'next/image'
 
 export default function Login() {
 	return (
-		<form className='flex w-fit flex-col gap-md rounded-md border p-lg'>
-			<div className='flex items-center justify-center'>
+		<form className='flex w-fit flex-col rounded-md border p-xl'>
+			<div className='flex items-center justify-center pb-lg'>
 				<Image src='/login-logo.svg' alt='digikala-mehr-logo' width={100} height={0} />
 			</div>
-			<h1 className='pt-4 font-bold'>ورود | ثبت نام</h1>
-			<span className='text-xs'>سلام!</span>
+			<h1 className='pb-lg text-xl font-bold'>ورود | ثبت نام</h1>
+			<span className='pb-sm text-xs'>سلام!</span>
 			<p className='pb-lg text-xs'>لطفا شماره موبایل یا ایمیل خود را وارد کنید</p>
 			<input type='text' className='h-12 rounded-md border border-solid border-red-600 px-lg py-sm outline-none' />
-			<span className='pb-lg text-xs text-red-600'>لطفا این قسمت را خالی نگذارید</span>
+			<span className='pb-xl pt-sm text-xs text-red-600'>لطفا این قسمت را خالی نگذارید</span>
 			<button className='mb-sm h-12 rounded-md border border-solid border-red-600 bg-red-600 px-lg py-sm text-white outline-none'>ورود</button>
-			<p className='text-xs'>ورود شما به معنای پذیرش شرایط دیجی کالا و قوانین حریم خصوصی است</p>
+			<p className='pt-sm text-xs'>ورود شما به معنای پذیرش شرایط دیجی کالا و قوانین حریم خصوصی است</p>
 		</form>
 	)
 }
