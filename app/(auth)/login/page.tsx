2281b9f9d9c258f7e54e12035653ae17eae38b07
mahdieh <madidgn82@gmail.com>
2025-02-24 18:47:41 +0330

    Add extend colors

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -10,18 +10,18 @@ export default function Login() {
 						<Image src='/svg/login-logo.svg' alt='digikala-mehr-logo' width={100} height={0} />
 					</div>
 					<h1 className='pb-xl text-lg font-bold text-black'>ورود | ثبت نام</h1>
-					<span className='pb-sm text-xs text-[#3f4064]'>سلام!</span>
-					<p className='pb-lg text-xs text-[#3f4064]'>لطفا شماره موبایل یا ایمیل خود را وارد کنید</p>
-					<input type='text' className='h-12 rounded-md border border-solid border-[#b2001a] px-md py-sm outline-none' />
-					<span className='pb-xl pt-sm text-xs text-[#b2001a]'>لطفا این قسمت را خالی نگذارید</span>
+					<span className='text-black-50 pb-sm text-xs'>سلام!</span>
+					<p className='text-black-50 pb-lg text-xs'>لطفا شماره موبایل یا ایمیل خود را وارد کنید</p>
+					<input type='text' className='h-12 rounded-md border border-solid border-red-200 px-md py-sm outline-none' />
+					<span className='pb-xl pt-sm text-xs text-red-200'>لطفا این قسمت را خالی نگذارید</span>
 					<Link
 						href='/create-referal'
-						className='mb-sm h-12 rounded-md border border-solid border-[#ef4056] bg-[#ef4056] px-md py-sm text-center text-white outline-none'
+						className='mb-sm h-12 rounded-md border border-solid border-red-100 bg-red-100 px-md py-sm text-center text-white outline-none'
 					>
 						ورود
 					</Link>
-					<p className='pt-md text-[11px] text-[#3f4064]'>
-						ورود شما به معنای پذیرش <span className='text-[#008eb2]'>شرایط دیجی کالا</span> و <span className='text-[#008eb2]'> حریم قوانین خصوصی </span>
+					<p className='text-black-50 pt-md text-[11px]'>
+						ورود شما به معنای پذیرش <span className='text-blue-500'>شرایط دیجی کالا</span> و <span className='text-blue-500'> حریم قوانین خصوصی </span>
 						است
 					</p>
 				</form>
