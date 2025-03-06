f82ec59ec7d5c65146306b097c40445f6e39f0b1
Saman Abbasi <samanabbasi.ir@gmail.com>
2025-03-06 16:45:46 +0330

    edit header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,12 +5,15 @@ import Link from 'next/link'
 
 function Header() {
 	return (
-		<header className='flex h-18 w-full items-center justify-between border border-b-4 px-md'>
-			<Image src='/header-logo.png' alt='digikala-mehr-logo' width={200} height={0} />
-			<Link href='/login'>
-				<button className='btn border'>ورود | ثبت نام</button>
-			</Link>
-		</header>
+		<>
+			<header className='header'>
+				<Image src='/header-logo.png' alt='digikala-mehr-logo' width={150} height={0} />
+				<Link href='/login'>
+					<button className='btn border'>ورود | ثبت نام</button>
+				</Link>
+			</header>
+			<div className='h-16 w-full lg:h-16'></div>
+		</>
 	)
 }
 
